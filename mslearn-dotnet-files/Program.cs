728e13d4c6690998edbe402eb5df2368cb96d175
auseta <alseta97@gmail.com>
2023-08-08 15:48:43 -0300

    Se creo el archivo Program.cs
    Se creó el archivo Program.cs que contiene toda la lógica de la aplicación

---
+++ mslearn-dotnet-files/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(Directory.GetCurrentDirectory(), "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindFiles(storesDirectory);
+
+var salesTotal = CalculateSalesTotal(salesFiles);    
+
+File.AppendAllText(Path.Combine(salesTotalDir, "total.txt"), $"{salesTotal}{Environment.NewLine}");
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        // The file name will contain the full path, so only check the end of it
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    foreach (var file in salesFiles)
+    {
+        string salesJson = File.ReadAllText(file);
+
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        salesTotal += data?.total ?? 0;
+    }
+
+    return salesTotal;
+}
+record SalesData (double total);
